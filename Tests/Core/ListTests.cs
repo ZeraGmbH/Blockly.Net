829348245e59bc0897a189309c00ac847278bc57
Dr. Jochen Manns <dev-2@psimarron.net>
2024-06-01 14:08:53 +0200

    Migrate list tests from IronBlock.

---
+++ Tests/Core/ListTests.cs
@@ -0,0 +1,237 @@
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+
+namespace BlocklyNetTests.Core;
+
+[TestFixture]
+public class ListTests : TestEnvironment
+{
+    [Test]
+    public async Task Can_Create_List()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_create_with"">
+                <mutation items=""3""></mutation>
+                <value name=""ADD0"">
+                <block type=""text"">
+                    <field name=""TEXT"">x</field>
+                </block>
+                </value>
+                <value name=""ADD1"">
+                <block type=""text"">
+                    <field name=""TEXT"">y</field>
+                </block>
+                </value>
+                <value name=""ADD2"">
+                <block type=""text"">
+                    <field name=""TEXT"">z</field>
+                </block>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo(new string[] { "x", "y", "z" }));
+    }
+
+    [Test]
+    public async Task Can_Split_List()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_split"">
+                <mutation mode=""SPLIT""></mutation>
+                <field name=""MODE"">SPLIT</field>
+                <value name=""INPUT"">
+                <block type=""text"">
+                    <field name=""TEXT"">x,y,z</field>
+                </block>
+                </value>
+                <value name=""DELIM"">
+                <shadow type=""text"">
+                    <field name=""TEXT"">,</field>
+                </shadow>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo(new string[] { "x", "y", "z" }));
+    }
+
+    [Test]
+    public async Task Can_Join_Lists()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_split"">
+                <mutation mode=""JOIN""></mutation>
+                <field name=""MODE"">JOIN</field>
+                <value name=""INPUT"">
+                <block type=""lists_create_with"">
+                    <mutation items=""3""></mutation>
+                    <value name=""ADD0"">
+                    <block type=""text"">
+                        <field name=""TEXT"">x</field>
+                    </block>
+                    </value>
+                    <value name=""ADD1"">
+                    <block type=""text"">
+                        <field name=""TEXT"">y</field>
+                    </block>
+                    </value>
+                    <value name=""ADD2"">
+                    <block type=""text"">
+                        <field name=""TEXT"">z</field>
+                    </block>
+                    </value>
+                </block>
+                </value>
+                <value name=""DELIM"">
+                <shadow type=""text"">
+                    <field name=""TEXT"">,</field>
+                </shadow>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo("x,y,z"));
+    }
+
+    [Test]
+    public async Task Can_Get_Length()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_length"">
+                <value name=""VALUE"">
+                <block type=""lists_split"">
+                    <mutation mode=""SPLIT""></mutation>
+                    <field name=""MODE"">SPLIT</field>
+                    <value name=""INPUT"">
+                    <block type=""text"">
+                        <field name=""TEXT"">a,b,c</field>
+                    </block>
+                    </value>
+                    <value name=""DELIM"">
+                    <shadow type=""text"">
+                        <field name=""TEXT"">,</field>
+                    </shadow>
+                    </value>
+                </block>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo(3));
+    }
+
+    [Test]
+    public async Task Can_Repeat()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_repeat"">
+                <value name=""ITEM"">
+                <block type=""text"">
+                    <field name=""TEXT"">hello</field>
+                </block>
+                </value>
+                <value name=""NUM"">
+                <shadow type=""math_number"">
+                    <field name=""NUM"">3</field>
+                </shadow>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo(new string[] { "hello", "hello", "hello" }));
+    }
+
+    [Test]
+    public async Task Can_Test_For_Empty()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_isEmpty"">
+                <value name=""VALUE"">
+                <block type=""lists_create_with"">
+                    <mutation items=""0""></mutation>
+                </block>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.True);
+    }
+
+    [Test]
+    public async Task Can_Find_Index()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <variables></variables>
+            <block type=""lists_indexOf"">
+                <field name=""END"">FIRST</field>
+                <value name=""VALUE"">
+                <block type=""lists_split"">
+                    <mutation mode=""SPLIT""></mutation>
+                    <field name=""MODE"">SPLIT</field>
+                    <value name=""INPUT"">
+                    <block type=""text"">
+                        <field name=""TEXT"">foo,bar,baz</field>
+                    </block>
+                    </value>
+                    <value name=""DELIM"">
+                    <shadow type=""text"">
+                        <field name=""TEXT"">,</field>
+                    </shadow>
+                    </value>
+                </block>
+                </value>
+                <value name=""FIND"">
+                <block type=""text"">
+                    <field name=""TEXT"">bar</field>
+                </block>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo(2));
+    }
+
+    [Test]
+    public async Task Can_Get_Element()
+    {
+        var script = Engine.Parser.Parse(@"
+            <xml xmlns=""http://www.w3.org/1999/xhtml"">
+            <block type=""lists_getIndex"">
+                <mutation statement=""false"" at=""true""></mutation>
+                <field name=""MODE"">GET</field>
+                <field name=""WHERE"">FROM_START</field>
+                <value name=""VALUE"">
+                <block type=""lists_split"">
+                    <mutation mode=""SPLIT""></mutation>
+                    <field name=""MODE"">SPLIT</field>
+                    <value name=""INPUT"">
+                    <block type=""text"">
+                        <field name=""TEXT"">foo,bar,baz</field>
+                    </block>
+                    </value>
+                    <value name=""DELIM"">
+                    <shadow type=""text"">
+                        <field name=""TEXT"">,</field>
+                    </shadow>
+                    </value>
+                </block>
+                </value>
+                <value name=""AT"">
+                <block type=""math_number"">
+                    <field name=""NUM"">2</field>
+                </block>
+                </value>
+            </block>
+            </xml>");
+
+        Assert.That(await script.Run(Site.Object), Is.EqualTo("bar"));
+    }
+}
