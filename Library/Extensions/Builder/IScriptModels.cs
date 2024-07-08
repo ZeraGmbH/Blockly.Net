3aa2f9f1dd650fb0ffed91bbc62e779cc67d483f
Dr. Jochen Manns <J.Manns@zera.de>
2024-07-08 13:05:45 +0200

    Support for custom blocks as models. (#11)
    * Custom blocks can now be marked as model-like and be referenced as types of model properties.
    
    * Must register by custom type not custom block type.
    
    * Value types should support Nullable variant as well.
    
    * Version bump.
    
    * Allow to register custom blocks as models.

--- Library/Extensions/Builder/IScriptModels.cs
+++ Library/Extensions/Builder/IScriptModels.cs
@@ -20,10 +20,10 @@ public interface IScriptModels
     /// <summary>
     /// Remember a model block.
     /// </summary>
-    /// <typeparam name="T">.NET model type.</typeparam>
+    /// <param name="modelType">.NET model type.</param>
     /// <param name="key">Blockly model type.</param>
     /// <param name="name">Display name of the model.</param>
-    void SetModel<T>(string key, string name) where T : class;
+    void SetModel(Type modelType, string key, string name);
 
     /// <summary>
     /// Remember a enumeration block.
@@ -32,4 +32,19 @@ public interface IScriptModels
     /// <param name="key">Blockly model type.</param>
     /// <param name="name">Display name of the enumeration.</param>
     void SetEnum<T>(string key, string name) where T : Enum;
+}
+
+/// <summary>
+/// Helper methods for the IScriptModels interface.
+/// </summary>
+public static class IScriptModelsExtensions
+{
+    /// <summary>
+    /// Remember a model block.
+    /// </summary>
+    /// <typeparam name="T">.NET model type.</typeparam>
+    /// <param name="models">Interface to extend.</param>
+    /// <param name="key">Blockly model type.</param>
+    /// <param name="name">Display name of the model.</param>
+    public static void SetModel<T>(this IScriptModels models, string key, string name) where T : class => models.SetModel(typeof(T), key, name);
 }
