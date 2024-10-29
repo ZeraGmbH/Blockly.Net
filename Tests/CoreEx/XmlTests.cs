d6c73dcd5f83c5d29eba9d398878313e31b80356
Dr. Jochen Manns <dev-2@psimarron.net>
2024-10-29 18:36:10 +0100

    Support for XML added.

--- Tests/CoreEx/XmlTests.cs
+++ Tests/CoreEx/XmlTests.cs
@@ -10,30 +10,30 @@ public class XmlTests : TestEnvironment
 {
     private const string Sample1 = @"<?xml version=""1.0"" encoding=""UTF-8""?>
         <CATALOG>
-        <CD mode=""oldie"">
-            <TITLE>Empire Burlesque</TITLE>
-            <ARTIST>Bob Dylan</ARTIST>
-            <COUNTRY>USA</COUNTRY>
-            <COMPANY>Columbia</COMPANY>
-            <PRICE>10.90</PRICE>
-            <YEAR>1985</YEAR>
-        </CD>
-        <CD>
-            <TITLE>Hide your heart</TITLE>
-            <ARTIST>Bonnie Tyler</ARTIST>
-            <COUNTRY>UK</COUNTRY>
-            <COMPANY>CBS Records</COMPANY>
-            <PRICE>9.90</PRICE>
-            <YEAR>1988</YEAR>
-        </CD>
-        <CD>
-            <TITLE>Greatest Hits</TITLE>
-            <ARTIST>Dolly Parton</ARTIST>
-            <COUNTRY>USA</COUNTRY>
-            <COMPANY>RCA</COMPANY>
-            <PRICE>9.90</PRICE>
-            <YEAR>1982</YEAR>
-        </CD>
+            <CD mode=""oldie"">
+                <TITLE>Empire Burlesque</TITLE>
+                <ARTIST>Bob Dylan</ARTIST>
+                <COUNTRY>USA</COUNTRY>
+                <COMPANY>Columbia</COMPANY>
+                <PRICE>10.90</PRICE>
+                <YEAR>1985</YEAR>
+            </CD>
+            <CD>
+                <TITLE>Hide your heart</TITLE>
+                <ARTIST>Bonnie Tyler</ARTIST>
+                <COUNTRY>UK</COUNTRY>
+                <COMPANY>CBS Records</COMPANY>
+                <PRICE>9.90</PRICE>
+                <YEAR>1988</YEAR>
+            </CD>
+            <CD>
+                <TITLE>Greatest Hits</TITLE>
+                <ARTIST>Dolly Parton</ARTIST>
+                <COUNTRY>USA</COUNTRY>
+                <COMPANY>RCA</COMPANY>
+                <PRICE>9.90</PRICE>
+                <YEAR>1982</YEAR>
+            </CD>
         </CATALOG>";
 
     [Test]
@@ -50,5 +50,42 @@ public class XmlTests : TestEnvironment
 
         /* Validate result. */
         Assert.That(file, Is.TypeOf<XmlFile>());
+    }
+
+    [Test]
+    public async Task Can_Query_Xml_Async()
+    {
+        /* Build block tree. */
+        var block = new QueryXmlDocument
+        {
+            Values = {
+                new() { Name = "FILE", Block = new CreateXmlDocument{ Values = { new() { Name = "CONTENT", Block = CreateStringBlock(Sample1) } } } },
+                new() { Name = "XPATH", Block = CreateStringBlock("CATALOG/CD[@mode=\"oldie\"]") }
+            }
+        };
+
+        /* Parse XML. */
+        var value = await block.EvaluateAsync(new Context(Site.Object));
+        var nodes = ((IEnumerable<XmlNode>)value!).ToList();
+
+        /* Validate result. */
+        Assert.That(nodes, Has.Count.EqualTo(1));
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(nodes[0].Name, Is.EqualTo("CD"));
+            Assert.That(nodes[0].LocalName, Is.EqualTo("CD"));
+            Assert.That(nodes[0].Namespace, Is.EqualTo(""));
+            Assert.That(nodes[0].Content, Is.EqualTo("Empire BurlesqueBob DylanUSAColumbia10.901985"));
+            Assert.That(nodes[0].Attributes, Has.Count.EqualTo(1));
+        });
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(nodes[0].Attributes[0].Name, Is.EqualTo("mode"));
+            Assert.That(nodes[0].Attributes[0].LocalName, Is.EqualTo("mode"));
+            Assert.That(nodes[0].Attributes[0].Namespace, Is.EqualTo(""));
+            Assert.That(nodes[0].Attributes[0].Value, Is.EqualTo("oldie"));
+        });
     }
 }
