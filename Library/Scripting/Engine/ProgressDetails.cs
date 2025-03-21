6f487057d9175b761e12a5f66c644d890e20b25f
Dr. Jochen Manns <J.Manns@zera.de>
2025-03-21 15:52:12 +0100

    Add progress details to each level of script.

--- Library/Scripting/Engine/ProgressDetails.cs
+++ Library/Scripting/Engine/ProgressDetails.cs
@@ -14,4 +14,9 @@ public class ProgressDetails
     /// Progress value between 0 and 1 - both inclusive.
     /// </summary>
     public double Progress { get; set; }
+
+    /// <summary>
+    /// The optional progress information depending on the type of script.
+    /// </summary>
+    public object? Info { get; set; }
 }
