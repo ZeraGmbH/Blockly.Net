07f0ba98502f418e321f6e2fc6867fcb12e86b9c
Dr. Jochen Manns <J.Manns@zera.de>
2025-04-01 14:23:54 +0200

    Proper OpenAPI definition of debug single step reasons.

--- Library/Scripting/Debugger/ScriptDebuggerStopReason.cs
+++ Library/Scripting/Debugger/ScriptDebuggerStopReason.cs
@@ -1,22 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace BlocklyNet.Scripting.Debugger;
 
 /// <summary>
 /// Reasons for the debugger to get informed.
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum ScriptDebuggerStopReason
 {
     /// <summary>
     /// Starting a block.
     /// </summary>
-    Enter,
+    Enter = 0,
 
     /// <summary>
     /// Block itself finished, next block will be started.
     /// </summary>
-    Leave,
+    Leave = 1,
 
     /// <summary>
     /// /// Block tree has finished.
     /// </summary>
-    Finish,
+    Finish = 2,
 }
