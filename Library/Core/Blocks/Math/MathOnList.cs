a6ee19e086a0aa1dfec84bbcce207f6aec58ff51
Dr. Jochen Manns <J.Manns@zera.de>
2025-04-23 15:21:04 +0200

    Support for custom numbers added.

--- Library/Core/Blocks/Math/MathOnList.cs
+++ Library/Core/Blocks/Math/MathOnList.cs
@@ -1,6 +1,7 @@
 
 
 using BlocklyNet.Core.Model;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BlocklyNet.Core.Blocks.Math;
 
@@ -14,34 +15,47 @@ public class MathOnList : Block
   /// <inheritdoc/>
   protected override async Task<object?> EvaluateAsync(Context context)
   {
+    var cvt = context.ServiceProvider.GetService<IDoubleExtractor>();
+
     var op = Fields["OP"];
     var list = await Values.EvaluateAsync<IEnumerable<object>>("LIST", context);
 
-    var doubleList = list.Select(x => (double)x).ToArray();
-
-    switch (op)
+    var doubleList = list.Select(raw =>
     {
-      case "SUM": return doubleList.Sum();
-      case "MIN": return doubleList.Min();
-      case "MAX": return doubleList.Max();
-      case "AVERAGE": return doubleList.Average();
-      case "MEDIAN": return Median(doubleList)!;
-      case "RANDOM": return doubleList.Length != 0 ? doubleList[rnd.Next(doubleList.Count())] : null!;
-      case "MODE": return doubleList.Length != 0 ? doubleList.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key : null!;
+      if (cvt == null) return (double)raw;
 
-      case "STD_DEV":
-        throw new NotImplementedException($"OP {op} not implemented");
+      /* Run converter - report cast error on mismatch. */
+      try
+      {
+        return cvt.GetNumber(raw);
+      }
+      catch (Exception)
+      {
+        return (double)raw;
+      }
+    }).ToArray();
 
-      default: throw new ApplicationException($"Unknown OP {op}");
-    }
+    return op switch
+    {
+      "AVERAGE" => doubleList.Average(),
+      "MAX" => doubleList.Max(),
+      "MEDIAN" => Median(doubleList)!,
+      "MIN" => doubleList.Min(),
+      "MODE" => doubleList.Length != 0 ? doubleList.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key : null!,
+      "RANDOM" => doubleList.Length != 0 ? doubleList[rnd.Next(doubleList.Length)] : null!,
+      "STD_DEV" => throw new NotImplementedException($"OP {op} not implemented"),
+      "SUM" => doubleList.Sum(),
+      _ => throw new ApplicationException($"Unknown OP {op}"),
+    };
   }
 
   private static object? Median(IEnumerable<double> values)
   {
-    if (!values.Any())
-      return null;
+    if (!values.Any()) return null;
+
     var sortedValues = values.OrderBy(x => x).ToArray();
     var mid = (sortedValues.Length - 1) / 2.0;
-    return (sortedValues[(int)(mid)] + sortedValues[(int)(mid + 0.5)]) / 2;
+
+    return (sortedValues[(int)(mid + 0.0)] + sortedValues[(int)(mid + 0.5)]) / 2;
   }
 }
