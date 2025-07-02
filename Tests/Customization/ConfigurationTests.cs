d2c33497c18b0fc27893d47bfb3c5d6886b951ac
Dr. Jochen Manns <dev-2@psimarron.net>
2025-07-02 19:48:30 +0200

    Provide test app to check for block JSON.

---
+++ Tests/Customization/ConfigurationTests.cs
@@ -0,0 +1,24 @@
+using System.Text.Json;
+using BlocklyNet.Extensions.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace BlocklyNetTests.Customization;
+
+[TestFixture]
+public class ConfigurationTests : TestEnvironment
+{
+    /// <summary>
+    /// Retrieve the overall configuration.
+    /// </summary>
+    [Test]
+    public void Can_Get_Configuration()
+    {
+        var provider = GetService<IConfigurationService>();
+        var anything = provider.Configuration;
+
+        var self = Path.Combine(Path.GetDirectoryName(typeof(ConfigurationTests).Assembly.Location)!, "..", "..", "..", "app", "config.json");
+
+        File.WriteAllText(self, JsonSerializer.Serialize(anything, new JsonSerializerOptions { WriteIndented = true }));
+    }
+}
