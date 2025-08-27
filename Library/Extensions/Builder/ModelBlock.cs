d2637fee5a5d1528ca9c2c2cad730557e03d9440
Dr. Jochen Manns <J.Manns@zera.de>
2025-08-27 15:24:21 +0200

    Added unsigned 16 bit numbers as well.

--- Library/Extensions/Builder/ModelBlock.cs
+++ Library/Extensions/Builder/ModelBlock.cs
@@ -99,6 +99,8 @@ public class ModelBlock<T> : Block where T : class, new()
         {typeof(byte?), new("Number","math_number","NUM","0")},
         {typeof(short), new("Number","math_number","NUM","0")},
         {typeof(short?), new("Number","math_number","NUM","0")},
+        {typeof(ushort), new("Number","math_number","NUM","0")},
+        {typeof(ushort?), new("Number","math_number","NUM","0")},
         /* String. */
         {typeof(string), new("String","text","TEXT","")},
         /* Object. */
